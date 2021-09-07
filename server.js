316c9e82505622083d83eda25e53286e8bcedce6
RamiZregat <rami.zregat97@gmail.com>
2021-09-07 22:03:21 +0300

    dont lab08

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const server = express();
 const PORT = process.env.PORT;
 const weatherdata = require("./Data/weather.json");
 const cors = require("cors");
+const { default: axios } = require("axios");
 server.use(cors());
 
 server.listen(PORT, () => {
@@ -13,20 +14,23 @@ server.listen(PORT, () => {
 });
 
 // http://localhost:3010/weather?lat=lat&lon=lon
-server.get("/weather", (req, res) => {
+server.get("/weather",async (req, res) => {
   const lon = req.query.lon;
   const lat = req.query.lat;
+  const Key = process.env.WEATHER_API_KEY;
+  const URL = `https://api.weatherbit.io/v2.0/forecast/daily?key=${Key}&lat=${lat}&lon=${lon}`;
+  let urlData = await axios.get(URL)
   let weatherArray=[];
-    const result = weatherdata.find(item =>{
-        if(item.lat === lat && item.lon === lon)
-        {
+    // const result = weatherdata.find(item =>{
+    //     if(item.lat === lat && item.lon === lon)
+    //     {
         
-         weatherArray=item.data.map(day => {
+         weatherArray=urlData.data.data.map(day => {
                 return new Forcast(day)
             })
-        }       
-    })
-    
+    //     }       
+    // })
+            // console.log(weatherArray);
     res.send(weatherArray);
 });
 
@@ -34,4 +38,28 @@ server.get("/weather", (req, res) => {
 function Forcast(day){
     this.date=day.valid_date
     this.description=`Low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`
+}
+
+// http://localhost:3010/movies?moviename=q
+server.get("/movies", async(req,res)=>{
+    const movieName = req.query.moviename;
+    const Key = process.env.MOVIE_API_KEY;
+    const URL = `https://api.themoviedb.org/3/search/movie?api_key=${Key}&query=${movieName}`
+    let moviesData= await axios.get(URL)
+    let moviesArray=[];
+    moviesArray=moviesData.data.results.map(item=>{
+        return new MoviesCreator(item)
+    })
+    res.send(moviesArray);
+
+})
+
+function MoviesCreator(movie){
+    this.title = movie.title
+    this.overview = movie.overview
+    this.average_votes = movie.vote_average
+    this.total_votes = movie.vote_count
+    this.image_url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    this.popularity = movie.popularity
+    this.released_on = movie.release_date
 }
